fe3f28b3b2d831cffebaecf0b7d55a40e6091979
Brendon Justin <brendonjustin@gmail.com>
2012-05-31 23:14:47 -0400

    Initial Commit

---
+++ EtherPlayer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EtherPlayer
+//
+//  Created by Brendon Justin on 5/31/12.
+//  Copyright (c) 2012 Naga Softworks, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
